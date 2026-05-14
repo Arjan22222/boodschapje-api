17f63559c3cd71b7466089d047a7d05b180afb75
Arjan22222 <asloot97@hotmail.com>
2026-05-14 14:49:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from supermarktconnector.ah import AHConnector
 from supermarktconnector.jumbo import JumboConnector
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -20,8 +21,6 @@ def zoek():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-  
